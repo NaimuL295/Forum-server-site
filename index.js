f386850d9b973de497ceaf050eb35e6fcb9acbb7
unknown <nayemk295@gmail.com>
2025-07-09 02:06:57 +0600

    start a basic server

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app=express()
 const port =process.env.PORT || 5000;
 
 
+const cors=require("cors")
+app.use("cors")
+app.use(express.json())
 app.get("/", (req, res) => {
     res.send(" server on");
 });
